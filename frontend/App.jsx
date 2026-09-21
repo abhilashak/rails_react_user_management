6358b4f8ff91da413f4b7e249213abe6085d9560
Abhilash A K <mailto.abhilashak@gmail.com>
2026-09-21 11:17:09 +0530

    add React user list

---
+++ frontend/App.jsx
@@ -0,0 +1,14 @@
+// frontend/App.jsx
+
+import UserList from "./components/UserList";
+
+function App() {
+  return (
+    <main>
+      <h1>Users</h1>
+      <UserList />
+    </main>
+  )
+}
+
+export default App;
---
+++ frontend/components/UserList.jsx
@@ -0,0 +1,11 @@
+// frontend/components/UserList.jsx
+
+function UserList() {
+  return (
+    <div>
+      <p>Users will appear here</p>
+    </div>
+  )
+}
+
+export default UserList;
